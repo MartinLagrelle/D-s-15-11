b49a028940aef3235efa8c6dce8f7bf34e10677d
Lagrelle Martin <lagrellemartin@gmail.com>
2023-11-16 11:44:17 +0100

    fait

--- js/lancer.js
+++ js/lancer.js
@@ -1,10 +1,9 @@
 function rollTheDice() {
-    setTimeout(function () {
+    
         var randomNumber = Math.floor(Math.random() * 6) + 1;
+        console.log(randomNumber)
 
 //source -> class im1 -> image du dès + numéro du 6 + png       
         document.querySelector(".img1").setAttribute("src",
-            "dice" + randomNumber + ".png");
-
-    })
+            "dice" + randomNumber + ".png");  
 }
